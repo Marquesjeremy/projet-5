608bfd0cd2c4330baab2f18d90fc81d3dc41a295
jeremy <126868845+Marquesjeremy@users.noreply.github.com>
2023-05-03 11:15:44 +0200

    ajouts des fleches G&D avec les bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,36 @@
 const slides =[
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image:"slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>",
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>",
 	},
 ];
 
 const arrowLeft = document.querySelector("#arrowLeft");
 arrowLeft.addEventListener("click", () => {
 	console.log("gauche");
-})
+});
 
 const arrowRight = document.querySelector("#arrowRight");
 arrowRight.addEventListener("click", () => {
 	console.log("droite");
-})
+});
 
 const dots = document.querySelector(".dots");
 dots.addEventListener("click", () => {
 	console.log("1");
-})
+});
 
 let dot;
 let index = 0;
@@ -46,3 +46,27 @@ for (let i = 0; slides.length > i; i++) {
 }
 
 numberDot[0].classList.add("dot_selected");
+
+const img = document.querySelector("#bannerImg");
+const textCaroussel = document.querySelector("#banner p");
+
+arrowLeft.addEventListener("click", () => {
+	numberDot[index].classList.remove("dot_selected");
+	if (index === 0) {
+		index = 4;
+		index--;
+		numberDot[index].classList.add("dot_selected");
+	} else {
+		index--;
+		numberDot[index].classList.add("dot_selected");
+	}
+});
+
+arrowRight.addEventListener("click", () => {
+	numberDot[index].classList.remove("dot_selected");
+	index++;
+	if (index === 4) {
+		index = 0;
+	}
+	numberDot[index].classList.add("dot_selected");
+});
